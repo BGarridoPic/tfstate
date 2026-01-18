90c5a35b6b5a939177d61ad6776ffc8dfd6c6221
bgarridopic <bgarridopic@stemdo.io>
2026-01-18 13:39:47 +0100

    Test 5

--- main.tf
+++ main.tf
@@ -7,19 +7,20 @@ terraform {
   }
 
   backend "s3" {
-    bucket         = "bgarrido-bucket-tf-state-1234567890"
+    bucket         = "bgarrido-bucket-tf-state-1234567890" # Coincide con el YAML
     key            = "prod/terraform.tfstate"
-    region         = "eu-west-3"
-    dynamodb_table = "terraform-lock-table"
+    region         = "eu-west-3"                           # Coincide con el YAML
+    dynamodb_table = "terraform-lock-table"                # Coincide con el YAML
     encrypt        = true
   }
 }
 
+# Nota: Tu Backend está en París (eu-west-3), pero tus recursos
+# se crearán en N. Virginia (us-east-1). Esto es válido.
 provider "aws" {
   region = "us-east-1"
 }
 
-
 resource "aws_s3_bucket" "mi_bucket_de_app" {
-  bucket = "bgarrido-app-bucket-1234567890"
+  bucket = "bgarrido-app-bucket-1234567890" # Nombre único para tu app
 }
