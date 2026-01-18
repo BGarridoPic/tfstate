3a5d4c234ff36e79b6d833dc0582b95f395e23b8
bgarridopic <bgarridopic@stemdo.io>
2026-01-18 13:25:48 +0100

    Test 1

---
+++ main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  backend "s3" {
+    bucket         = "BGarrido-Bucket-TF-State-1234567890"
+    key            = "prod/terraform.tfstate"
+    region         = "eu-west-3"
+    dynamodb_table = "terraform-lock-table"
+    encrypt        = true
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+resource "aws_s3_bucket" "mi_bucket_de_app" {
+  bucket = "bgarrido-app-bucket-1234567890"
+}
